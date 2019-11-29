eb3135c65721fd0e8a329fd5ec5272e54f13ed2e
Gerard Hynes <gkhynes@gmail.com>
2019-11-29 22:52:11 +0000

    Connect drum pads to audio files

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -4,8 +4,36 @@ import "./DrumPad.css";
 export default class DrumPad extends Component {
   constructor(props) {
     super(props);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.playSound = this.playSound.bind(this);
   }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+  handleKeyPress(e) {
+    if (e.keyCode === this.props.keyCode) {
+      this.playSound();
+    }
+  }
+
+  playSound(e) {
+    const audio = document.getElementById(this.props.keyTrigger);
+    audio.cuurrentTime = 0;
+    audio.play();
+  }
+
   render() {
-    return <div className="pad">{this.props.keyTrigger}</div>;
+    return (
+      <div className="pad" id={this.props.id} onClick={this.playSound}>
+        <audio src={this.props.clip} id={this.props.keyTrigger}></audio>
+        {this.props.keyTrigger}
+      </div>
+    );
   }
 }
