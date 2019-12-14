a946b41a5b2d02e4ae08834f18531ea8ff460d3e
Gerard Hynes <gkhynes@gmail.com>
2019-12-14 12:40:25 +0000

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import DrumMachine from "./DrumMachine";
+import Footer from "./Footer";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <DrumMachine />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="Footer">
+      Made by <a href="https://github.com/GK-Hynes">Ger Hynes</a>
+    </footer>
+  );
+}
